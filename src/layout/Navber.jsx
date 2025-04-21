2e6ae48a847080ac5b60186876bcb08051089d97
Your Name <islamshamim1819@gmail.com>
2025-04-21 14:44:58 +0600

    header & footer

--- src/layout/Navber.jsx
+++ src/layout/Navber.jsx
@@ -9,7 +9,7 @@ export default function Navber() {
         <li className='text-xl font-bold mr-2'><NavLink to="/form">Add Transaction</NavLink></li>
     </>
     return (
-        <div className="navbar bg-base-200">
+        <div className="navbar bg-blue-200">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
